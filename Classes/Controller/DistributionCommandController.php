788348e20ae12a9897bf31f9d1919abebaccc4e4
Michiel Roos <michiel@maxserv.nl>
2015-04-24 22:29:09 +0200

    Add description to list of distributions Download extensionlist if no extensions are found in the repository

--- Classes/Controller/DistributionCommandController.php
+++ Classes/Controller/DistributionCommandController.php
@@ -34,6 +34,14 @@ use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 class DistributionCommandController extends CommandController {
 
 	/**
+	 * Configuration utillity
+	 *
+	 * @var \TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility
+	 * @inject
+	 */
+	protected $configurationUtility;
+
+	/**
 	 * Distribution repository
 	 *
 	 * @var \TYPO3\CMS\Extensionmanager\Domain\Repository\ExtensionRepository
@@ -42,22 +50,163 @@ class DistributionCommandController extends CommandController {
 	protected $distributionRepository;
 
 	/**
-	 * List backend users
+	 * Extension download utillity
+	 *
+	 * @var \TYPO3\CMS\Extensionmanager\Utility\DownloadUtility
+	 * @inject
+	 */
+	protected $downloadUtility;
+
+	/**
+	 * Extension management service
+	 *
+	 * @var \TYPO3\CMS\Extensionmanager\Service\ExtensionManagementService
+	 * @inject
+	 */
+	protected $managementService;
+
+	/**
+	 * Repository Helper
+	 *
+	 * @var \TYPO3\CMS\Extensionmanager\Utility\Repository\Helper
+	 * @inject
+	 */
+	protected $repositoryHelper;
+
+	/**
+	 * List distributions
 	 *
 	 * @return void
 	 */
 	public function listCommand() {
+		if ($this->distributionRepository->countAll() === 0) {
+			$this->updateExtensionList();
+		}
 		$distributions = $this->distributionRepository->findAllOfficialDistributions();
 		$this->outputLine('Found ' . count($distributions) . ' official distributions');
 		/** @var Extension $distribution */
 		foreach ($distributions as $distribution) {
-			$this->outputLine($distribution->getExtensionKey());
+			$this->outputLine($distribution->getExtensionKey() . ' - ' . $distribution->getTitle());
 		}
+		$this->outputLine();
 		$distributions = $this->distributionRepository->findAllCommunityDistributions();
 		$this->outputLine('Found ' . count($distributions) . ' community distributions');
 		/** @var Extension $distribution */
 		foreach ($distributions as $distribution) {
-			$this->outputLine($distribution->getExtensionKey());
+			$this->outputLine($distribution->getExtensionKey() . ' - ' . $distribution->getTitle());
+		}
+	}
+
+	/**
+	 * Install command
+	 *
+	 * @param string $distributionKey The distribution key
+	 *
+	 * @return void
+	 */
+	public function installCommand($distributionKey = '') {
+		if ($this->distributionRepository->countAll() === 0) {
+			$this->updateExtensionList();
+		}
+		$distributionToInstall = NULL;
+		if ($distributionKey !== '') {
+			$distributions = $this->distributionRepository->findAllOfficialDistributions();
+			/** @var Extension $distribution */
+			foreach ($distributions as $distribution) {
+				if ($distributionKey == $distribution->getExtensionKey()) {
+					$distributionToInstall = $distribution;
+					break;
+				}
+			}
+			$this->outputLine();
+			$distributions = $this->distributionRepository->findAllCommunityDistributions();
+			/** @var Extension $distribution */
+			foreach ($distributions as $distribution) {
+				if ($distributionKey == $distribution->getExtensionKey()) {
+					$distributionToInstall = $distribution;
+					break;
+				}
+			}
+			if ($distributionToInstall === NULL) {
+				$this->outputLine('The specified distribution wat not found. Please select one of the distributions shown by the distribution:list action.');
+			}
+			$this->installDistribution($distributionToInstall);
+		} else {
+			$this->outputLine('Please specify an distribution key using the --distribution-key option.');
+		}
+	}
+
+	/**
+	 * Action for installing a distribution -
+	 * redirects directly to configuration after installing
+	 *
+	 * @param \TYPO3\CMS\Extensionmanager\Domain\Model\Extension $extension The
+	 *    extension to install
+	 *
+	 * @return void
+	 */
+	private function installDistribution(\TYPO3\CMS\Extensionmanager\Domain\Model\Extension $extension) {
+		list($result, $errorMessages) = $this->installFromTer($extension);
+		if ($errorMessages) {
+			foreach ($errorMessages as $extensionKey => $messages) {
+				foreach ($messages as $message) {
+					$this->outputLine($message['message']);
+				}
+			}
+		} else {
+			$this->outputLine(
+				\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('distribution.welcome.message', 'extensionmanager') .
+				' ' .
+				$extension->getExtensionKey()
+			);
+		}
+	}
+
+	/**
+	 * Install an action from TER
+	 * Downloads the extension, resolves dependencies and installs it
+	 *
+	 * @param \TYPO3\CMS\Extensionmanager\Domain\Model\Extension $extension The
+	 *    extension to install
+	 * @param string $downloadPath The downloadpath
+	 *
+	 * @return array
+	 */
+	protected function installFromTer(\TYPO3\CMS\Extensionmanager\Domain\Model\Extension $extension, $downloadPath = 'Local') {
+		$result = FALSE;
+		$errorMessages = array();
+		try {
+			$this->downloadUtility->setDownloadPath($downloadPath);
+//			$this->managementService->setAutomaticInstallationEnabled($this->configurationUtility->getCurrentConfiguration('extensionmanager')['automaticInstallation']['value']);
+			if (($result = $this->managementService->installExtension($extension)) === FALSE) {
+				$errorMessages = $this->managementService->getDependencyErrors();
+			}
+		} catch (\TYPO3\CMS\Extensionmanager\Exception\ExtensionManagerException $e) {
+			$errorMessages = array(
+				$extension->getExtensionKey() => array(
+					array(
+						'code' => $e->getCode(),
+						'message' => $e->getMessage(),
+					)
+				),
+			);
+		}
+
+		return array($result, $errorMessages);
+	}
+
+	/**
+	 * Update extension list.
+	 *
+	 * @return void
+	 */
+	protected function updateExtensionList() {
+		$this->outputLine('Updating extensionlist.');
+		try {
+			$this->repositoryHelper->updateExtList();
+		} catch (\TYPO3\CMS\Extensionmanager\Exception\ExtensionManagerException $e) {
+			$errorMessage = $e->getMessage();
+			$this->outputLine($errorMessage);
 		}
 	}
 }
